3298049ee4c97fcebe0a506bc3c73ba70f76b542
heronimus <heronimustra@gmail.com>
2017-11-19 16:50:15 +0700

    Add FPS Camera and HUD to Level 2 Scene
    Add FPS Camera and HUD, also arrange asset object on Level 2 Scene

---
+++ Assets/Script/HUD.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HUD : MonoBehaviour {
+
+	public Text itemListText;
+
+	void Start(){	
+
+	}
+
+	public void addItemList(string itemname){
+		itemListText.text += "\n"+itemname ;
+	}
+
+
+}
---
+++ Assets/Script/MainPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayer : MonoBehaviour {
+
+	HUD hudObject;
+	bool itemsInTouch;
+	GameObject pickUpObject;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Player Start");
+		hudObject = GameObject.FindGameObjectWithTag ("HUD").GetComponent<HUD> ();
+		itemsInTouch = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(itemsInTouch && Input.GetKeyDown("e")){
+			Debug.Log ("PickUp !");
+			hudObject.addItemList ("- Book");
+			pickUpObject.SetActive (false);
+
+		}
+		if (Input.GetKeyDown("space"))
+			print("space key was pressed");
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.tag.Equals ("Items")) {
+			itemsInTouch = true;
+			pickUpObject = collision.gameObject;
+			Debug.Log ("Sentuh");
+		}
+	}
+	void OnCollisionExit(Collision collision){
+		if (collision.gameObject.tag.Equals ("Items")) {
+			Debug.Log ("Tidak Sentuh");
+		}
+	}
+}
