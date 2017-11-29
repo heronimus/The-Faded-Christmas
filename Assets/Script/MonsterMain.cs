2e05bf5a8accd66e590ab5a847169443db883bc8
heronimus <heronimustra@gmail.com>
2017-11-29 21:05:52 +0700

    Add Monster Krampus
    Add Monster Krampus and it's Wandering System

---
+++ Assets/Script/MonsterMain.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMain : MonoBehaviour {
+
+
+	public GameObject monsterObject;
+	Animation monsterAnim;
+	MonsterWandering monsterWander;
+
+	// Use this for initialization
+	void Start () {
+		monsterAnim = monsterObject.GetComponent<Animation> ();
+		monsterWander = GetComponent<MonsterWandering> ();
+
+		monsterAnim.wrapMode= WrapMode.Loop;
+		monsterAnim.CrossFade("sleep");
+		monsterWander.StopWandering ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
